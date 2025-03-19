0f64e021da38596fe0c311e2507e5fcfa92979df
minato10chan <134562110+minato10chan@users.noreply.github.com>
2025-03-19 01:08:10 +0000

    se

--- use_pysqlite3.py
+++ use_pysqlite3.py
@@ -2,4 +2,9 @@ import pysqlite3
 import sys
 sys.modules['sqlite3'] = pysqlite3
 
+import streamlit as st
+
+# シークレットキーを読み込む
+secret_key = st.secrets["general"]["secret_key"]
+
 # ...existing code...
